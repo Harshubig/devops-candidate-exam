b5eeb923f81c47ceecf0f08a7e1e4bb83d6fc8da
Harshubig <109741583+Harshubig@users.noreply.github.com>
2023-04-14 11:38:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_internet_gateway" "gw" {
 
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = "true"
   tags = {
@@ -36,7 +36,7 @@ resource "aws_subnet" "public_subnet" {
 
 resource "aws_subnet" "private_subnet" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = "false"
   tags = {
@@ -48,7 +48,7 @@ resource "aws_subnet" "private_subnet" {
 
 resource "aws_subnet" "public_subnet1" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.0.0/24"
+  cidr_block              = "10.0.3.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = "true"
   tags = {
@@ -61,7 +61,7 @@ resource "aws_subnet" "public_subnet1" {
 
 resource "aws_subnet" "private_subnet1" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.3.0/24"
+  cidr_block              = "10.0.4.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = "false"
   tags = {
